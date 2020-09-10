9497d7525e42208d225a64a6e21611589e80d3b0
Jsoneft <jasonleft@qq.com>
2020-09-10 20:02:10 +0800

    add designed db part

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "ginblog_backend/routes"
+import (
+	"ginblog_backend/model"
+	"ginblog_backend/routes"
+)
 
 func main()  {
+	// 引用数据库
+	model.InitDb()
 	routes.InitRouter()
 }
