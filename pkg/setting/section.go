8e54d221cdd30d6d8dd0a7b68b34bf6738823497
Jsoneft <jasonleft@qq.com>
2021-01-21 12:49:00 +0800

    add configs

---
+++ pkg/setting/section.go
@@ -0,0 +1,41 @@
+package setting
+
+import (
+	"time"
+)
+
+type ServerSettingS struct {
+	RunMode      string
+	HttpPort     string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+type AppSettingS struct {
+	DefaultPageSize int
+	MaxPageSize     int
+	LogSavePath     string
+	LogFileName     string
+	LogFileExt      string
+}
+
+type DatabaseSettingS struct {
+	DBType       string
+	UserName     string
+	PassWord     string
+	Host         string
+	DBName       string
+	TablePrefix  string
+	Charset      string
+	ParseTime    bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+func (a *Setting) ReadSection(k string, v interface{}) error {
+	err := a.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ pkg/setting/setting.go
@@ -0,0 +1,19 @@
+package setting
+
+import "github.com/spf13/viper"
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSettings() (*Setting, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")
+	vp.AddConfigPath("configs/")
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	return &Setting{vp}, nil
+}
