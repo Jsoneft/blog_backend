8e29bd066809cbe65227611ec83045db4c0b5897
Jsoneft <jasonleft@qq.com>
2021-01-22 16:43:30 +0800

    add logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -151,45 +151,45 @@ func (l *Logger) Debug(v ...interface{}) {
 	l.WithLevel(LevelDebug).Output(fmt.Sprint(v...))
 }
 
-func (l Logger) Debugf(format string, v ...interface{}) {
+func (l *Logger) Debugf(format string, v ...interface{}) {
 	l.WithLevel(LevelDebug).Output(fmt.Sprintf(format, v...))
 }
 
-func (l Logger) Info(v ...interface{}) {
+func (l *Logger) Info(v ...interface{}) {
 	l.WithLevel(LevelInfo).Output(fmt.Sprint(v...))
 }
 
-func (l Logger) Infof(format string, v ...interface{}) {
+func (l *Logger) Infof(format string, v ...interface{}) {
 	l.WithLevel(LevelInfo).Output(fmt.Sprintf(format, v...))
 }
 
-func (l Logger) Warn(v ...interface{}) {
+func (l *Logger) Warn(v ...interface{}) {
 	l.WithLevel(LevelWarn).Output(fmt.Sprint(v...))
 }
 
-func (l Logger) Warnf(format string, v ...interface{}) {
+func (l *Logger) Warnf(format string, v ...interface{}) {
 	l.WithLevel(LevelWarn).Output(fmt.Sprintf(format, v...))
 }
 
-func (l Logger) Error(v ...interface{}) {
+func (l *Logger) Error(v ...interface{}) {
 	l.WithLevel(LevelError).Output(fmt.Sprint(v...))
 }
 
-func (l Logger) Errorf(format string, v ...interface{}) {
+func (l *Logger) Errorf(format string, v ...interface{}) {
 	l.WithLevel(LevelError).Output(fmt.Sprintf(format, v...))
 }
 
-func (l Logger) Fatal(v ...interface{}) {
+func (l *Logger) Fatal(v ...interface{}) {
 	l.WithLevel(LevelFatal).Output(fmt.Sprint(v...))
 }
-func (l Logger) Fatalf(format string, v ...interface{}) {
+func (l *Logger) Fatalf(format string, v ...interface{}) {
 	l.WithLevel(LevelFatal).Output(fmt.Sprintf(format, v...))
 }
 
-func (l Logger) Panic(v ...interface{}) {
+func (l *Logger) Panic(v ...interface{}) {
 	l.WithLevel(LevelPanic).Output(fmt.Sprint(v...))
 }
 
-func (l Logger) Panicf(format string, v ...interface{}) {
+func (l *Logger) Panicf(format string, v ...interface{}) {
 	l.WithLevel(LevelPanic).Output(fmt.Sprintf(format, v...))
 }
