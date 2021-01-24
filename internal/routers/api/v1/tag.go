f6513e70298e2a26bd29199069462d3dc978e4e2
Jsoneft <jasonleft@qq.com>
2021-01-25 03:16:36 +0800

    feat: add comment to v1/tag.go to fit in swagger-UI support

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -1,8 +1,6 @@
 package v1
 
 import (
-	"ginblog_backend/pkg/app"
-	"ginblog_backend/pkg/errcode"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,12 +10,46 @@ func NewTag() Tag {
 	return Tag{}
 }
 
-func (t Tag) Get(c *gin.Context) {}
-func (t Tag) List(c *gin.Context) {
-	// test
-	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
-	return
-}
+// @Summary 获取多个标签
+// @Produce json
+// @Param name query string false "标签名称" maxlength(100)
+// @Param state query int false "状态" Enums(0, 1) default(1)
+// @Param page query int false "页码"
+// @Param page_size query int false "每页数量"
+// @Success 200 {Object} model.TagSwagger "成功"
+// @Failure 400 {Object} errcode.Error "请求错误"
+// @Failure 500 {Object} errcode.Error "内部错误"
+// @Router /api/v1/tag [get]
+func (t Tag) List(c *gin.Context) {}
+
+// @Summary 更新标签
+// @Produce json
+// @Param id path int true "标签ID"
+// @Param name body string ture "标签名称" minlength(3) maxlength(100)
+// @Param state body int false "状态" Enums(0, 1) default(1)
+// @Param modified_by body string false "修改者" minlength(3) maxlength(100)
+// @Success 200 {Object} model.Tag "成功"
+// @Failure 400 {Object} errcode.Error "请求错误"
+// @Failure 500 {Object} errcode.Error "内部错误"
+// @Router /api/v1/tags/{id} [put]
 func (t Tag) Update(c *gin.Context) {}
+
+// @Summary 删除标签
+// @Produce json
+// @Param id path int true "标签ID"
+// @Success 200 {Object} string "成功"
+// @Failure 400 {Object} errcode.Error "请求错误"
+// @Failure 500 {Object} errcode.Error "内部错误"
+// @Router /api/v1/tags/{id} [delete]
 func (t Tag) Delete(c *gin.Context) {}
+
+// @Summary 新增标签
+// @Produce json
+// @Param name body string true "标签名称" minlength(3) maxlength(100)
+// @Param state body int false "状态" Enums(0, 1) default(1)
+// @Param created_by body string false "创建者" minlength(3) maxlength(100)
+// @Success 200 {Object} model.Tag "成功"
+// @Failure 400 {Object} errcode.Error "请求错误"
+// @Failure 500 {Object} errcode.Error "内部错误"
+// @Router /api/v1/tag [post]
 func (t Tag) Create(c *gin.Context) {}
