e7b56756d973eb20bde63dc4812b04de476c664e
Jsoneft <jasonleft@qq.com>
2021-02-01 00:50:45 +0800

    feat: Add Article && ArticleTag API.

--- internal/model/article.go
+++ internal/model/article.go
@@ -29,3 +29,76 @@ func (a Article) Create(db *gorm.DB) (*Article, error) {
 	}
 	return &a, nil
 }
+
+func (a Article) Update(db *gorm.DB, values interface{}) error {
+	if err := db.Model(&a).Where("id = ? AND is_del = ?", a.ID, 0).Updates(&values).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a Article) Delete(db *gorm.DB) error {
+	if err := db.Model(&Article{}).Where("id = ? AND is_del = ?", a.ID, 0).Delete(&a).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a Article) Get(db *gorm.DB) (*Article, error) {
+	var article Article
+	db = db.Model(&Article{}).Where("id = ? AND state = ? AND is_del = ?", a.ID, a.State, 0)
+	if err := db.First(&article).Error; err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return &article, nil
+}
+
+type ArticleRow struct {
+	ArticleID     uint32
+	TagID         uint32
+	TagName       string
+	ArticleTitle  string
+	ArticleDesc   string
+	CoverImageUrl string
+	Content       string
+}
+
+// 通过tagID 拿到文章
+func (a Article) ListByTagID(db *gorm.DB, tagID uint32, pageOffset, pageSize int) ([]*ArticleRow, error) {
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	fields := []string{"ar.id AS article_id", "ar.title AS article_title", "ar.desc AS article_desc", "ar.cover_image_url", "ar.content"}
+	fields = append(fields, []string{"t.id AS tag_id", "t.name AS tag_name"}...)
+	rows, err := db.Select(fields).Table(ArticleTag{}.TableName()+"AS at").
+		Joins("LEFT JOIN `"+Tag{}.TableName()+"` AS t ON at.tag_id = t.id").
+		Joins("LEFT JOIN `"+Article{}.TableName()+"` AS ar ON at.article_id = ar.id").
+		Where("at.`tag_id` = ? AND ar.state = ? AND ar.is_del = ?", tagID, a.State, 0).Rows()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var articleRows []*ArticleRow
+	for rows.Next() {
+		r := ArticleRow{}
+		if err := rows.Scan(&r.ArticleID, &r.ArticleTitle, &r.ArticleDesc, &r.TagID, &r.TagName); err != nil {
+			return nil, err
+		}
+		articleRows = append(articleRows, &r)
+	}
+	return articleRows, nil
+}
+
+func (a Article) CountByTagId(db *gorm.DB, tagID uint32) (int, error) {
+	var cnt int
+	err := db.Table(ArticleTag{}.TableName()+"AS at").
+		Joins("LEFT JOIN `"+Tag{}.TableName()+"`AS t ON at.tag_id = t.id").
+		Joins("LEFT JOIN `"+Article{}.TableName()+"` AS ar ON at.article_id = ar.id").
+		Where("at.tag_id = ? AND ar.state = ? AND ar.is_del = ?", tagID, a.State, 0).
+		Count(&cnt).Error
+	if err != nil {
+		return 0, err
+	}
+	return cnt, nil
+}
--- internal/model/article_tag.go
+++ internal/model/article_tag.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type ArticleTag struct {
 	*Model
 	TagId     uint32 `json:"tag_id"`
@@ -8,4 +10,49 @@ type ArticleTag struct {
 
 func (t ArticleTag) TableName() string {
 	return "blog_article_tag"
+}
+
+func (t ArticleTag) GetByAID(db *gorm.DB) (ArticleTag, error) {
+	var articleTag ArticleTag
+	if err := db.Model(&t).Where("article_id = ? AND is_del = ?", t.ArticleId, 0).First(&articleTag).Error; err != nil && nil != gorm.ErrRecordNotFound {
+		return ArticleTag{}, err
+	}
+	return articleTag, nil
+}
+
+func (t ArticleTag) ListByTID(db *gorm.DB) ([]*ArticleTag, error) {
+	var articleTags []*ArticleTag
+	if err := db.Model(&t).Where("tag_id = ? AND is_del = ?", t.TagId, 0).Find(&articleTags).Error; err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	return articleTags, nil
+}
+
+func (t ArticleTag) Create(db *gorm.DB) error {
+	if err := db.Model(&t).Create(&t).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// UpdateOne updates the row connected to the updated Article.
+func (t ArticleTag) UpdateOne(db *gorm.DB, values interface{}) error {
+	if err := db.Model(&t).Where("article_id = ? AND is_del = ?", t.ID, 0).Limit(1).Updates(&values).Error; err != nil && err != gorm.ErrRecordNotFound {
+		return err
+	}
+	return nil
+}
+
+func (t ArticleTag) Delete(db *gorm.DB) error {
+	if err := db.Model(&t).Where("id = ? AND is_del = ?", t.ID, 0).Delete(&t).Error; err != nil && err != gorm.ErrRecordNotFound {
+		return err
+	}
+	return nil
+}
+
+func (t ArticleTag) DeleteOne(db *gorm.DB) error {
+	if err := db.Model(&t).Where("article_id = ? AND is_del = ?", t.ArticleId, 0).Delete(&t).Limit(1).Error; err != nil {
+		return err
+	}
+	return nil
 }
